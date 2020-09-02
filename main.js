7ed1f1173723b05c01803108a482382d3b3c5050
Allred <andrewrallred@gmail.com>
2020-09-02 17:03:01 -0400

    watch the sample queries folder, and pick the appropriate data from within the returned json to convert to our model

--- main.js
+++ main.js
@@ -3,42 +3,73 @@ const QuickType = require('./converters/quicktype-converter.js');
 
 const fs = require('fs');
 const util = require('util');
+var _ = require('lodash');
 
 // Convert fs.readFile into Promise version of same    
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-function loadQuery() {
-    return readFile(__dirname + '/sample-queries/essays.graphql');
-}
-
 function writeClass(className, classDefinition) {
     writeFile(__dirname + '/generated-classes/' + className + '.cs', classDefinition);
 }
 
-async function main() {
-    let data = await loadQuery();
+function main() {
+
+      fs.watch('./sample-queries', (eventType, filename) => {
+            console.log(filename + ' updated: ' + eventType);
+            generateModelFromQuery(filename);
+        });
+}
+
+async function generateModelFromQuery(file) {
+
+    let modelName = file.replace('.graphql', '');
+    
+    let data = await readFile(__dirname + '/sample-queries/' + file);;
     console.log('query data loaded from disk');
     let query = {
         query: data.toString(),
-        operationName: "EssayQuery",
+        operationName: null,
         variables: null
     };
-    
+
+    console.log('running query');
+
     let queryResults = await CmsService.postQuery(query);
 
-    let essay = queryResults.data.data;    
+    console.log('query successful');
 
-    const { lines: cSharpQuery } = await QuickType.quicktypeJSON(
-         "csharp",
-         "essay",
-         JSON.stringify(essay)
-       );
+    let queryResultsData = queryResults.data.data; 
+    if (queryResults.data.errors == null) {
+        
+        let objectToConvert = null;
+        if (queryResultsData[_.camelCase(modelName)] != null) {
+            objectToConvert = queryResultsData[_.camelCase(modelName)];
+        } 
+        else if (queryResultsData["search"] != null) {
+            objectToConvert = queryResultsData.search;
+        }
+        else {
+            // TODO throw an error here
+        }
 
-      console.log('Essay C# Class');
+        console.log('converting query to c# class');
+        const { lines: cSharpClassDefinition } = await QuickType.quicktypeJSON(
+            "csharp",
+            modelName,
+            JSON.stringify(objectToConvert)
+        );
 
-      let classDefinition = cSharpQuery.join("\n");
-      writeClass("Essay", classDefinition);
+        console.log('saving c# class');
+
+        let classDefinition = cSharpClassDefinition.join("\n");
+        writeClass(modelName, classDefinition);
+    }
+    else {
+        console.log(queryResults.data.errors);
+    }
+
+    
 }
 
 main();
