5075281b09b7c437236d41c115bc9226e2682479
Allred <andrewrallred@gmail.com>
2020-10-22 13:46:24 -0400

    adjustments and cleaning up test models

--- models/Essay.cs
+++ models/Essay.cs
@@ -51,12 +51,10 @@ namespace Dororin.Essay
         public string PublishingInformation { get; set; }
 
         [JsonProperty("importedId")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long ImportedId { get; set; }
+        public string ImportedId { get; set; }
 
         [JsonProperty("PostId")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long PostId { get; set; }
+        public string PostId { get; set; }
 
         [JsonProperty("code")]
         public string Code { get; set; }
@@ -140,37 +138,5 @@ namespace Dororin.Essay
                 new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
             },
         };
-    }
-
-    internal class ParseStringConverter : JsonConverter
-    {
-        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            if (String.IsNullOrEmpty(value?.ToString())) value = default(long).ToString();
-            long l;
-            if (Int64.TryParse(value, out l))
-            {
-                return l;
-            }
-            throw new Exception("Cannot unmarshal type long");
-        }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-        {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
-                return;
-            }
-            var value = (long)untypedValue;
-            serializer.Serialize(writer, value.ToString());
-            return;
-        }
-
-        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
     }
 }
